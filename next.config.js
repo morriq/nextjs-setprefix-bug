05f46c91e2fd2c7d65028d3ba788f2a4a05856b0
Shaun Warman <shaunwarman1@gmail.com>
2024-01-08 09:40:59 -0600

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true
+  },
+  distDir: '.build',
+  basePath: '/myapp',
+  assetPrefix: 'https://cdn.staticprefix.com/abc/123/'
+}
 
 module.exports = nextConfig
